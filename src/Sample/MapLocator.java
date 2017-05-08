18c0c0eeb51b2f163b192e24da2956cf80d5a9e0
oskarw90 <Oskar.wallin90@hotmail.com>
2017-05-08 23:16:43 +0200

    argh
    Signed-off-by: oskarw90 <Oskar.wallin90@hotmail.com>

--- src/Sample/MapLocator.java
+++ src/Sample/MapLocator.java
@@ -157,4 +157,377 @@ public class MapLocator extends JFrame {
         this.validate();
     }
 }
+/** TODO FIX UP CLASSES */
+
+
+class MapLocator$CategoryListener implements ActionListener {
+    MapLocator$CategoryListener(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        JPanel categoryPanel = new JPanel();
+        categoryPanel.setLayout(new BoxLayout(categoryPanel, 1));
+        JPanel row1 = new JPanel();
+        this.this$0.add(row1);
+        row1.add(new JLabel("Category name: "));
+        MapLocator.access$1(this.this$0, new JTextField(15));
+        row1.add(MapLocator.access$2(this.this$0));
+        categoryPanel.add(row1);
+        JPanel row2 = new JPanel();
+        this.this$0.add(row2);
+        row2.add(new JLabel("Choose a colour: "));
+        this.this$0.jcc = new JColorChooser();
+        row2.add(this.this$0.jcc);
+        categoryPanel.add(row2);
+        int answer = JOptionPane.showConfirmDialog((Component)null, categoryPanel, "New category", 2, 3);
+        if(answer == 0) {
+            String str = MapLocator.access$2(this.this$0).getText();
+            Color clr = this.this$0.jcc.getColor();
+            Category newCat = new Category(str, clr);
+            this.this$0.catList.add(newCat);
+            this.this$0.dataModel.removeAllElements();
+
+            for(int i = 0; i < this.this$0.catList.size(); ++i) {
+                Category tempCat = (Category)this.this$0.catList.get(i);
+                this.this$0.dataModel.addElement(tempCat.getName());
+            }
+        } else if(answer == 2) {
+            JOptionPane.showMessageDialog((Component)null, "Category not added");
+        }
+
+    }
+}
+class MapLocator$DeleteCategoryListener implements ActionListener {
+    MapLocator$DeleteCategoryListener(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        int selectedIndex = this.this$0.category.getSelectedIndex();
+        if(selectedIndex >= 0) {
+            String str = (String)this.this$0.category.getSelectedValue();
+            this.this$0.dataModel.remove(selectedIndex);
+
+            for(int i = 0; i < this.this$0.catList.size(); ++i) {
+                Category tmpCat = (Category)this.this$0.catList.get(i);
+                String search = tmpCat.getName();
+                if(search == str) {
+                    Collection<Place> tmpPlace = this.this$0.where.values();
+                    Iterator iter = tmpPlace.iterator();
+
+                    while(iter.hasNext()) {
+                        Place nextPlace = (Place)iter.next();
+                        if(nextPlace.getCat() != null && nextPlace.getCat() == tmpCat) {
+                            iter.remove();
+                            this.this$0.pp.remove(nextPlace);
+                        }
+                    }
+
+                    this.this$0.catList.remove(i);
+                    this.this$0.repaint();
+                }
+            }
+        }
+
+    }
+}
+
+class MapLocator$DeletePlacesListener implements ActionListener {
+    MapLocator$DeletePlacesListener(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        Collection<Place> tmpPlace = this.this$0.where.values();
+        Iterator iter = tmpPlace.iterator();
+
+        while(iter.hasNext()) {
+            Place nextPlace = (Place)iter.next();
+            nextPlace.setShowed(false);
+            this.this$0.pp.remove(nextPlace);
+            iter.remove();
+            this.this$0.pp.repaint();
+        }
+
+    }
+}
+
+class MapLocator$DescribedPlaceListener extends MouseAdapter {
+    MapLocator$DescribedPlaceListener(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void mouseClicked(MouseEvent mev) {
+        int x = mev.getX();
+        int y = mev.getY();
+        String which = (String)this.this$0.category.getSelectedValue();
+        Category cate = null;
+
+        String search;
+        for(int i = 0; i < this.this$0.catList.size(); ++i) {
+            Category tmpCat = (Category)this.this$0.catList.get(i);
+            search = tmpCat.getName();
+            if(search == which) {
+                cate = (Category)this.this$0.catList.get(i);
+            }
+        }
+
+        newDescribedPlace newDP = new newDescribedPlace();
+        String name = newDP.getName();
+        search = newDP.getDescription();
+        DescribedPlace dp = new DescribedPlace(x - 19, y - 38, name, search, cate);
+        Position pos = new Position(x, y);
+        this.this$0.where.put(pos, dp);
+        this.this$0.pp.add(dp);
+        dp.repaint();
+        dp.validate();
+        this.this$0.pp.removeMouseListener(this.this$0.describedLyss);
+        this.this$0.pp.setCursor(Cursor.getDefaultCursor());
+    }
+}
+class MapLocator$ExitAction implements ActionListener {
+    MapLocator$ExitAction(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        int answer = JOptionPane.showConfirmDialog((Component)null, "Do you wish to quit without saving?", "Exiting application", 0);
+        if(answer == 0) {
+            System.exit(0);
+        }
+
+    }
+}
+
+class MapLocator$HideCategoryListener implements ActionListener {
+    MapLocator$HideCategoryListener(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        int selectedIndex = this.this$0.category.getSelectedIndex();
+        if(selectedIndex >= 0) {
+            String str = (String)this.this$0.category.getSelectedValue();
+
+            for(int i = 0; i < this.this$0.catList.size(); ++i) {
+                Category tmpCat = (Category)this.this$0.catList.get(i);
+                String search = tmpCat.getName();
+                if(search == str) {
+                    Collection<Place> tmpPlace = this.this$0.where.values();
+                    Iterator iter = tmpPlace.iterator();
+
+                    while(iter.hasNext()) {
+                        Place nextPlace = (Place)iter.next();
+                        if(nextPlace.getCat() != null && nextPlace.getCat() == tmpCat) {
+                            nextPlace.setShowed(false);
+                            this.this$0.pp.repaint();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+class MapLocator$HidePlace implements ActionListener {
+    MapLocator$HidePlace(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        Collection<Place> tmpPlace = this.this$0.where.values();
+        Iterator iter = tmpPlace.iterator();
+
+        while(iter.hasNext()) {
+            Place nextPlace = (Place)iter.next();
+            nextPlace.setShowed(false);
+            this.this$0.pp.repaint();
+        }
+
+    }
+}
+
+
+class MapLocator$MarkedPlace extends MouseAdapter {
+    MapLocator$MarkedPlace(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void mouseClicked(MouseEvent mev) {
+        if(mev.getButton() == 1) {
+            Place p = (Place)mev.getSource();
+            if(!p.isMarked()) {
+                p.setMarked(true);
+            } else if(p.isMarked()) {
+                p.setMarked(false);
+            }
+        }
+
+    }
+}
+
+class MapLocator$MouseListener implements ActionListener {
+    MapLocator$MouseListener(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        Object c = ave.getSource();
+        if(c == MapLocator.access$3(this.this$0)) {
+            JComboBox cb = (JComboBox)ave.getSource();
+            String passed = (String)cb.getSelectedItem();
+            if(passed.equals("Named Place")) {
+                this.this$0.pp.addMouseListener(this.this$0.namedLyss);
+                this.this$0.pp.setCursor(Cursor.getPredefinedCursor(1));
+            }
+
+            if(passed.equals("Described Place")) {
+                this.this$0.pp.addMouseListener(this.this$0.describedLyss);
+                this.this$0.pp.setCursor(Cursor.getPredefinedCursor(1));
+            }
+        }
+
+    }
+}
+
+public class MapLocator$NamedPlaceListener extends MouseAdapter {
+    public MapLocator$NamedPlaceListener(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void mouseClicked(MouseEvent mev) {
+        int x = mev.getX();
+        int y = mev.getY();
+        String which = (String)this.this$0.category.getSelectedValue();
+        Category cate = null;
+
+        for(int i = 0; i < this.this$0.catList.size(); ++i) {
+            Category tmpCat = (Category)this.this$0.catList.get(i);
+            String search = tmpCat.getName();
+            if(search == which) {
+                cate = (Category)this.this$0.catList.get(i);
+            }
+        }
+
+        newNamedPlace nmp = new newNamedPlace();
+        String name = nmp.getName();
+        NamedPlace np = new NamedPlace(x - 19, y - 38, name, cate);
+        this.this$0.pp.add(np);
+        Position pos = new Position(x, y);
+        this.this$0.where.put(pos, np);
+        np.repaint();
+        np.validate();
+        this.this$0.pp.removeMouseListener(this.this$0.namedLyss);
+        this.this$0.pp.setCursor(Cursor.getDefaultCursor());
+    }
+}
+
+class MapLocator$NewAction implements ActionListener {
+    MapLocator$NewAction(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        int answer = this.this$0.jfc.showOpenDialog(this.this$0);
+        if(answer == 0) {
+            File file = this.this$0.jfc.getSelectedFile();
+            String filename = file.getAbsolutePath();
+            if(this.this$0.pp != null) {
+                this.this$0.remove(this.this$0.pp);
+            }
+
+            this.this$0.pp = new PicturePanel(filename);
+            this.this$0.add(this.this$0.pp);
+            this.this$0.pack();
+            this.this$0.validate();
+            this.this$0.repaint();
+        }
+    }
+}
+class MapLocator$OpenAction implements ActionListener {
+    MapLocator$OpenAction(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        int state = this.this$0.jfc.showOpenDialog((Component)null);
+        File file = this.this$0.jfc.getSelectedFile();
+        if(file != null && state == 0) {
+            JOptionPane.showMessageDialog((Component)null, file.getPath());
+        } else if(state == 1) {
+            JOptionPane.showMessageDialog((Component)null, "Canceled");
+        }
+
+    }
+}
+class MapLocator$SaveAction extends WindowAdapter {
+    MapLocator$SaveAction(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void windowClosing(WindowEvent e) {
+        int answer = JOptionPane.showConfirmDialog((Component)null, "Do you wish to quit without saving?", "Exiting application", 0);
+        if(answer == 0) {
+            this.this$0.setDefaultCloseOperation(3);
+        } else {
+            this.this$0.jfc.showSaveDialog((Component)null);
+            this.this$0.jfc.getSelectedFile().getPath();
+            this.this$0.unsaved = Boolean.valueOf(false);
+            this.this$0.unsavednew = Boolean.valueOf(false);
+        }
+
+    }
+}
+
+class MapLocator$SaveButtonAction implements ActionListener {
+    MapLocator$SaveButtonAction(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+        this.this$0.jfc.showSaveDialog((Component)null);
+        this.this$0.jfc.getSelectedFile().getPath();
+        this.this$0.unsaved = Boolean.valueOf(false);
+        this.this$0.unsavednew = Boolean.valueOf(false);
+    }
+}
+
+class MapLocator$SearchListener implements ActionListener {
+    MapLocator$SearchListener(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent ave) {
+    }
+}
+
+class MapLocator$SearchTextAction extends MouseAdapter {
+    MapLocator$SearchTextAction(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void mouseClicked(MouseEvent mev) {
+        MapLocator.access$0(this.this$0).setText("");
+    }
+}
+class MapLocator$ShowedPlace extends MouseAdapter {
+    MapLocator$ShowedPlace(MapLocator var1) {
+        this.this$0 = var1;
+    }
+
+    public void mouseClicked(MouseEvent mev) {
+        if(mev.getButton() == 2) {
+            Place p = (Place)mev.getSource();
+            if(!p.isShowed()) {
+                p.setShowed(true);
+            } else if(p.isShowed()) {
+                p.setShowed(false);
+            }
+        }
+
+    }
+}
+
 
